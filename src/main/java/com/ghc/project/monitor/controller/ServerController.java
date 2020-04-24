87901efee37def84f87144a984185ac2c74e724c
chengshuang <chengshuang@ghcchina.com.cn>
2020-04-24 10:08:43 +0800

    升级诺依版本

---
+++ src/main/java/com/ghc/project/monitor/controller/ServerController.java
@@ -0,0 +1,28 @@
+package com.ghc.project.monitor.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.ghc.framework.web.controller.BaseController;
+import com.ghc.framework.web.domain.AjaxResult;
+import com.ghc.framework.web.domain.Server;
+
+/**
+ * 服务器监控
+ * 
+ * @author ruoyi
+ */
+@RestController
+@RequestMapping("/monitor/server")
+public class ServerController extends BaseController
+{
+    @PreAuthorize("@ss.hasPermi('monitor:server:list')")
+    @GetMapping()
+    public AjaxResult getInfo() throws Exception
+    {
+        Server server = new Server();
+        server.copyTo();
+        return AjaxResult.success(server);
+    }
+}
