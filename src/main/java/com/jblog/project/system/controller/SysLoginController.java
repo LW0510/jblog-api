12b76b0789ebc8e2824fa4bd41982f23b1a46c30
LW0510 <2581992919@qq.com>
2020-05-15 21:12:58 +0800

    修复了项目初始化的一些错误

--- src/main/java/com/jblog/project/system/controller/SysLoginController.java
+++ src/main/java/com/jblog/project/system/controller/SysLoginController.java
@@ -49,7 +49,7 @@ public class SysLoginController
      * @return 结果
      */
     @PostMapping("/login")
-    public AjaxResult login(@RequestBody LoginBody loginBody)
+    public AjaxResult login(LoginBody loginBody)
     {
         AjaxResult ajax = AjaxResult.success();
         // 生成令牌
